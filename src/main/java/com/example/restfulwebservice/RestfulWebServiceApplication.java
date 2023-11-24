0df695363f157f5b7f08376582c8f88aa3d3d5ca
ksm <ksmles36@imokorea.co.kr>
2023-11-24 12:36:39 +0900

    asd

--- src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfulWebServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServiceApplication.class, args);
+    }
 
 }
