209dc8728fd2b03aa33ad94073425f239539ea68
ksm <ksmles36@imokorea.co.kr>
2023-11-26 22:01:29 +0900

    231125

---
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -0,0 +1,30 @@
+package com.example.restfulwebservice.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserDaoService userDaoService;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return userDaoService.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
+    }
+
+
+}
