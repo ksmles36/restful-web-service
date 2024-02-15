b383894592a5cb99568c2c94d7953fef764d391c
ksmles36 <ksmles36@gmail.com>
2024-02-15 23:01:16 +0900

    push 안되는 이슈 테스트

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -36,6 +36,7 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-internationalized")
     public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
         return messageSource.getMessage("greeting.message", null, locale);
+
     }
 
 }
