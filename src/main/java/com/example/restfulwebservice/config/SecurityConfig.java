f5452b3c2f5351c9aa0c7ece275d64d6aba8e495
ksmles36 <ksmles36@gmail.com>
2024-03-10 20:52:04 +0900

    jpa 초기데이터 생성

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -1,32 +1,40 @@
-//package com.example.restfulwebservice.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    UserDetailsService userDetailsService(){
-//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-//
-//        UserDetails newUser = User.withUsername("user")
-//                .password(passwordEncoder().encode("pwpwpw@@"))
-//                .authorities("read")
-//                .build();
-//
-//        userDetailsManager.createUser(newUser);
-//        return userDetailsManager;
-//    }
-//
-//    @Bean
-//    BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//}
+package com.example.restfulwebservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails newUser = User.withUsername("user")
+                .password(passwordEncoder().encode("pwpwpw@@"))
+                .authorities("read")
+                .build();
+
+        userDetailsManager.createUser(newUser);
+        return userDetailsManager;
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, join_date, name, password, ssn, grade) values (1001, now(), 'User1', 'test111', '111111-1111111', 'vip');
+insert into users(id, join_date, name, password, ssn, grade) values (1002, now(), 'User2', 'test222', '111111-1111112', 'normal');
+insert into users(id, join_date, name, password, ssn, grade) values (1003, now(), 'User3', 'test333', '111111-1111113', 'normal');
